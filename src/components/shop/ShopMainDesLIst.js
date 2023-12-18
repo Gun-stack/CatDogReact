e16c953f737a1a5f33db96b70c1583a52f6eac08
Gun-stack <fkdlfo@naver.com>
2023-12-18 10:40:43 +0900

    22
    22

--- src/components/shop/ShopMainDesLIst.js
+++ src/components/shop/ShopMainDesLIst.js
@@ -5,14 +5,89 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { useNavigate } from "react-router";
-
+import SwalCustomAlert from "../Alerts/SwalCustomAlert";
 
 
 function ShopMainDesLIst() {
     const dispatch = useDispatch();
     const shopInfo = useSelector((state) => state.shop);
     const desList = useSelector((state) => state.desList);
+    const user = useSelector((state) => state.user);
+
     const navigate = useNavigate();
+
+    const [editable, setEditable] = useState(false);
+    const [editedPosition, setEditedPosition] = useState('');
+    const [editableDesId, setEditableDesId] = useState(null);
+
+
+
+
+
+    const handleEditClick = (desId) => {
+        setEditableDesId(desId);
+        // Optionally, initialize editedPosition with the current des.position or other default value
+    };
+
+    const handleDeleteClick = async (desId) => {
+        
+        const formData = new FormData();
+        formData.append("num", desId);
+        formData.append("sId", shopInfo.sid)
+        await Swal.fire({
+            html: '<img src="/img/logo/withdraw.png" alt="회원 탈퇴이미지" className="withdraw-img"/>',
+            title: '<span class="sweet-modal-title">디자이너를 삭제하시겠습니까?</span>',
+            showCancelButton: true,
+            confirmButtonText: '삭제',
+            confirmButtonColor: '#F9950F',
+            cancelButtonText: '취소',
+        })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    axios.post("http://localhost:8090/shopoutdes", formData)
+                        .then(() => {
+                            navigate(0);
+                        })
+                }
+
+            })
+
+        setEditableDesId("");
+
+        // Optionally, initialize editedPosition with the current des.position or other default value
+    };
+
+    const handleSaveClick = (async (desId) => {
+        console.log("editedPosition : " + editedPosition);
+        console.log("desId : " + desId);
+        if (editedPosition === "" || editedPosition === null) {
+            SwalCustomAlert(
+                'fail',
+                '직책을 입력하세요!!',
+            );
+            setEditableDesId(null);
+        } else {
+            const formData = new FormData();
+            formData.append("num", desId);
+            formData.append("position", editedPosition);
+            await axios.post("http://localhost:8090/updateposition", formData)
+                .then(() => {
+                    setEditableDesId(null);
+                    setEditedPosition("");
+                    navigate(0);
+                });
+        }
+
+
+        // Implement logic to save the editedPosition for the specific stylist
+
+    });
+
+    const handleCancelClick = (desId) => {
+        setEditableDesId(null);
+        setEditedPosition("");
+        // Optionally, reset editedPosition to the original value
+    };
 
     useEffect(() => {
         console.log(shopInfo);
@@ -55,7 +130,7 @@ function ShopMainDesLIst() {
                 if (result.isConfirmed) {
                     console.log("2번 모달!!");
                     const desId = result.value;
-                    axios.get(`http://localhost:8090/desinfobyid?desId=${desId}`)
+                    axios.get(`http://localhost:8090/selectdesbyid?desId=${desId}`)
                         .then(async (res) => {
                             await Swal.fire({
                                 title: res.data.name,
@@ -88,12 +163,18 @@ function ShopMainDesLIst() {
                                                     navigate(0);
                                                 })
                                             })
-                                    } else {
-                                        console.log("false 고정?");
                                     }
                                 })
 
 
+
+                        }).catch((err) => {
+                            Swal.fire({
+                                html: '<img src="/img/logo/modal_notice_logo.png"/></span>',
+                                title: '<span class="sweet-modal-title">등록되지 않은 디자이너 입니다.</span>',
+                                confirmButtonColor: '#F9950F',
+                                confirmButtonText: '확인',
+                            });
                         })
                 }
             }).catch((err) => {
@@ -114,6 +195,7 @@ function ShopMainDesLIst() {
 
             {desList.map((des) => (
 
+
                 <div className="stylelist-content" key={des.num} >
                     <div className="st-profile-container">
                         <div className="st-profile-img">
@@ -121,14 +203,47 @@ function ShopMainDesLIst() {
                         </div>
 
                         <div className="st-profile-context">
-                            <div className="st-profile-name">{des.position}  {des.desNickname}</div>
+                            <div className="st-profile-name">
+                                {editableDesId === des.num ? (
+                                    <input
+                                        type="text"
+                                        value={editedPosition}
+                                        onChange={(e) => setEditedPosition(e.target.value)}
+                                    />
+                                ) : (
+                                    `${des.position}`
+                                )}
+                            </div>
+                            <div className="st-profile-name">{des.desNickname}</div>
                             <div className="st-profile-shop">{shopInfo.name}</div>
                             <div className="st-profile-info">{des.info}</div>
                         </div>
                     </div>
-
                     <div className="st-button-container">
-                        <a href="#"><button className="st-button">편집<i className="fas fa-pen btn-icon"></i></button></a>
+
+                        {shopInfo.sid === des.sid && user.roles === "ROLE_SHOP" && (
+                            <button className="st-button" onClick={() => handleDeleteClick(des.num)}>
+                                삭제<i className="fas fa-pen btn-icon"></i>
+                            </button>
+                        )}
+
+                        {editableDesId === des.num ? (
+                            <>
+                                <button className="st-button" onClick={() => handleSaveClick(des.num)}>
+                                    저장<i className="fas fa-check btn-icon"></i>
+                                </button>
+                                <button className="st-button" onClick={handleCancelClick}>
+                                    취소<i className="fas fa-times btn-icon"></i>
+                                </button>
+
+                            </>
+
+                        ) : (
+
+                            <button className="st-button" onClick={() => handleEditClick(des.num)}>
+                                편집<i className="fas fa-pen btn-icon"></i>
+                            </button>
+                        )}
                         <Link to={`/shop/${shopInfo.num}/reservation/${des.num}`} ><button className="st-button">예약하기<i className="far fa-calendar-alt btn-icon"></i></button></Link>
                     </div>
                 </div>
